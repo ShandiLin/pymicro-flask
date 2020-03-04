6d803b398ada9a3b5c473778d9e25a6fb3d2ade2
LinShauHsuan <cindyboy220@gmail.com>
2020-03-04 21:30:18 +0800

    add travis, modify test and uwsgi.ini

---
+++ docker/test.sh
@@ -0,0 +1,18 @@
+set -x
+
+DATA='{"hello":"pymicro-flask"}'
+GET=$(curl -d "$DATA" -s $PYMICRO/infranext/odin)
+
+RET_CODE=$(echo $GET | python -c "import sys, json; print(json.load(sys.stdin)['pymicro.ret'])")
+[[ $RET_CODE == 0 | $RET_CODE == 1 ]]
+
+
+if [ "$PYMICRO_SEND_METRICS" = "true" ]; then
+    METRICS=$(curl -s $PYMICRO_STATSD_WEB/metrics | grep -i pymicro_)
+    UWSGI_METRICS=$(curl -s $PYMICRO_STATSD_WEB/metrics | grep -i uwsgi_pymicro_)
+    [[ ! -z "$METRICS" ]]
+    [[ ! -z "$UWSGI_METRICS" ]]
+else
+    METRICS=$(curl -s $PYMICRO/metrics | grep -i pymicro_)
+    [[ ! -z "$METRICS" ]]
+fi
